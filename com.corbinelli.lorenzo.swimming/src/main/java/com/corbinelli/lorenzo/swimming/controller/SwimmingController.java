e9cbe278db4565b78bfe03536abd2e404e5346c7
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2024-06-11 16:15:11 +0200

    Implemented allSwimmers in the controller

--- com.corbinelli.lorenzo.swimming/src/main/java/com/corbinelli/lorenzo/swimming/controller/SwimmingController.java
+++ com.corbinelli.lorenzo.swimming/src/main/java/com/corbinelli/lorenzo/swimming/controller/SwimmingController.java
@@ -2,8 +2,16 @@ package com.corbinelli.lorenzo.swimming.controller;
 
 public class SwimmingController {
 
+	private SwimmerView swimmerView;
+	private SwimmerRepository swimmerRepository;
+
+	public SwimmingController(SwimmerView swimmerView, SwimmerRepository swimmerRepository) {
+		this.swimmerView = swimmerView;
+		this.swimmerRepository = swimmerRepository;
+	}
+	
 	public void allSwimmers() {
-		
+		swimmerView.showAllSwimmers(swimmerRepository.findAll());
 	}
 
 }
