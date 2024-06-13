d3c42b1ce675a046d604eae2fd535ab73bf301b4
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2024-06-13 11:39:04 +0200

    Added the id label and textBox to the view

--- com.corbinelli.lorenzo.swimming/src/main/java/com/corbinelli/lorenzo/swimming/view/swing/SwimmerSwingView.java
+++ com.corbinelli.lorenzo.swimming/src/main/java/com/corbinelli/lorenzo/swimming/view/swing/SwimmerSwingView.java
@@ -9,11 +9,17 @@ import javax.swing.border.EmptyBorder;
 
 import com.corbinelli.lorenzo.swimming.model.Swimmer;
 import com.corbinelli.lorenzo.swimming.view.SwimmerView;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
+import java.awt.Insets;
 
 public class SwimmerSwingView extends JFrame implements SwimmerView {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField txtId;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -29,12 +35,36 @@ public class SwimmerSwingView extends JFrame implements SwimmerView {
 	}
 
 	public SwimmerSwingView() {
+		setTitle("Swimming App");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblId = new JLabel("id");
+		GridBagConstraints gbc_lblId = new GridBagConstraints();
+		gbc_lblId.insets = new Insets(0, 0, 0, 5);
+		gbc_lblId.anchor = GridBagConstraints.EAST;
+		gbc_lblId.gridx = 1;
+		gbc_lblId.gridy = 0;
+		contentPane.add(lblId, gbc_lblId);
+		
+		txtId = new JTextField();
+		txtId.setName("idTextBox");
+		GridBagConstraints gbc_txtId = new GridBagConstraints();
+		gbc_txtId.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtId.gridx = 2;
+		gbc_txtId.gridy = 0;
+		contentPane.add(txtId, gbc_txtId);
+		txtId.setColumns(10);
 	}
 
 	@Override
