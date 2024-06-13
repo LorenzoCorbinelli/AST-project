94ace414f5f0a2be9215b4efa05433545496ec90
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2024-06-13 11:43:16 +0200

    Added name label and textBox to the view

--- com.corbinelli.lorenzo.swimming/src/main/java/com/corbinelli/lorenzo/swimming/view/swing/SwimmerSwingView.java
+++ com.corbinelli.lorenzo.swimming/src/main/java/com/corbinelli/lorenzo/swimming/view/swing/SwimmerSwingView.java
@@ -20,6 +20,8 @@ public class SwimmerSwingView extends JFrame implements SwimmerView {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtId;
+	private JLabel lblName;
+	private JTextField txtName;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -43,28 +45,47 @@ public class SwimmerSwingView extends JFrame implements SwimmerView {
 
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
 		JLabel lblId = new JLabel("id");
 		GridBagConstraints gbc_lblId = new GridBagConstraints();
-		gbc_lblId.insets = new Insets(0, 0, 0, 5);
-		gbc_lblId.anchor = GridBagConstraints.EAST;
-		gbc_lblId.gridx = 1;
+		gbc_lblId.fill = GridBagConstraints.VERTICAL;
+		gbc_lblId.insets = new Insets(0, 0, 5, 5);
+		gbc_lblId.gridx = 2;
 		gbc_lblId.gridy = 0;
 		contentPane.add(lblId, gbc_lblId);
 		
 		txtId = new JTextField();
 		txtId.setName("idTextBox");
 		GridBagConstraints gbc_txtId = new GridBagConstraints();
+		gbc_txtId.insets = new Insets(0, 0, 5, 0);
 		gbc_txtId.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtId.gridx = 2;
+		gbc_txtId.gridx = 3;
 		gbc_txtId.gridy = 0;
 		contentPane.add(txtId, gbc_txtId);
 		txtId.setColumns(10);
+		
+		lblName = new JLabel("name");
+		GridBagConstraints gbc_lblName = new GridBagConstraints();
+		gbc_lblName.anchor = GridBagConstraints.EAST;
+		gbc_lblName.insets = new Insets(0, 0, 5, 5);
+		gbc_lblName.gridx = 2;
+		gbc_lblName.gridy = 1;
+		contentPane.add(lblName, gbc_lblName);
+		
+		txtName = new JTextField();
+		txtName.setName("nameTextBox");
+		GridBagConstraints gbc_txtName = new GridBagConstraints();
+		gbc_txtName.insets = new Insets(0, 0, 5, 0);
+		gbc_txtName.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtName.gridx = 3;
+		gbc_txtName.gridy = 1;
+		contentPane.add(txtName, gbc_txtName);
+		txtName.setColumns(10);
 	}
 
 	@Override
