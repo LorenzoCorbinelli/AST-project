2a6a1a0210f7edf80f30d3a9bf105594acee63f8
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2024-06-15 10:41:33 +0200

    Implemented allSwimmers IT

---
+++ com.corbinelli.lorenzo.swimming/src/it/java/com/corbinelli/lorenzo/swimming/view/swing/SwimmerSwingViewIT.java
@@ -0,0 +1,62 @@
+package com.corbinelli.lorenzo.swimming.view.swing;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.swing.annotation.GUITest;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.testcontainers.containers.MongoDBContainer;
+
+import com.corbinelli.lorenzo.swimming.controller.SwimmingController;
+import com.corbinelli.lorenzo.swimming.model.Swimmer;
+import com.corbinelli.lorenzo.swimming.repository.mongo.SwimmerMongoRepository;
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+
+public class SwimmerSwingViewIT extends AssertJSwingJUnitTestCase {
+	
+	@ClassRule
+	public static final MongoDBContainer mongo = new MongoDBContainer("mongo:4.4.3");
+	private MongoClient client;
+	private FrameFixture window;
+	private SwimmerSwingView swimmerView;
+	private SwimmerMongoRepository swimmerRepository;
+	private SwimmingController swimmingController;
+
+	@Override
+	protected void onSetUp() throws Exception {
+		client = new MongoClient(new ServerAddress(mongo.getHost(), mongo.getFirstMappedPort()));
+		swimmerRepository = new SwimmerMongoRepository(client, "testDB", "testCollection");
+		for (Swimmer swimmer : swimmerRepository.findAll()) {
+			swimmerRepository.delete(swimmer.getId());
+		}
+		GuiActionRunner.execute(() -> {
+			swimmerView = new SwimmerSwingView();
+			swimmingController = new SwimmingController(swimmerView, swimmerRepository);
+			swimmerView.setSwimmingController(swimmingController);
+			return swimmerView;
+		});
+		window = new FrameFixture(robot(), swimmerView);
+		window.show();
+	}
+	
+	@Override
+	protected void onTearDown() throws Exception {
+		client.close();
+	}
+
+	@Test @GUITest
+	public void testAllSwimmers() {
+		Swimmer swimmer1 = new Swimmer("1", "test1", "testGender", "testStroke");
+		Swimmer swimmer2 = new Swimmer("2", "test2", "testGender", "testStroke");
+		swimmerRepository.save(swimmer1);
+		swimmerRepository.save(swimmer2);
+		GuiActionRunner.execute(() -> swimmingController.allSwimmers());
+		assertThat(window.list("swimmerList").contents())
+			.containsExactly(swimmer1.toString(), swimmer2.toString());
+	}
+}
