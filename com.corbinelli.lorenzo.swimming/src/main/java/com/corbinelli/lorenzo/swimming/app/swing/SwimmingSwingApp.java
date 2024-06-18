4b0b88c8bd37286004bef1b72e02ee27005641fe
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2024-06-18 10:41:34 +0200

    Used Picocli to manage the args parameter

--- com.corbinelli.lorenzo.swimming/src/main/java/com/corbinelli/lorenzo/swimming/app/swing/SwimmingSwingApp.java
+++ com.corbinelli.lorenzo.swimming/src/main/java/com/corbinelli/lorenzo/swimming/app/swing/SwimmingSwingApp.java
@@ -1,6 +1,9 @@
 package com.corbinelli.lorenzo.swimming.app.swing;
 
 import java.awt.EventQueue;
+import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.corbinelli.lorenzo.swimming.controller.SwimmingController;
 import com.corbinelli.lorenzo.swimming.repository.mongo.SwimmerMongoRepository;
@@ -8,29 +11,46 @@ import com.corbinelli.lorenzo.swimming.view.swing.SwimmerSwingView;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 
-public class SwimmingSwingApp {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(mixinStandardHelpOptions = true)
+public class SwimmingSwingApp implements Callable<Void>{
+	
+	@Option(names = {"--mongo-host"}, description = "MongoDB host address")
+	private String mongoHost = "localhost";
+	
+	@Option(names = {"--mongo-port"}, description = "MongoDB host port")
+	private int mongoPort = 27017;
+	
+	@Option(names = {"--db-name"}, description = "Database name")
+	private String databaseName = "swimming";
+	
+	@Option(names = {"--db-collection"}, description = "Collection name")
+	private String collectionName = "swimmer";
 
 	public static void main(String[] args) {
+		new CommandLine(new SwimmingSwingApp()).execute(args);
+	}
+	
+	@Override
+	public Void call() throws Exception {
 		EventQueue.invokeLater(() -> {
 			try {
-				String mongoHost = "localhost";
-				int mongoPort = 27017;
-				if(args.length > 0)
-					mongoHost = args[0];
-				if(args.length > 1)
-					mongoPort = Integer.parseInt(args[1]);
 				SwimmerMongoRepository swimmerRepository = 
 						new SwimmerMongoRepository(
-								new MongoClient(new ServerAddress(mongoHost, mongoPort)),
-								"swimming", "swimmer");
-				SwimmerSwingView swimmerView = new SwimmerSwingView();
-				SwimmingController swimmingController = new SwimmingController(swimmerView, swimmerRepository);
-				swimmerView.setSwimmingController(swimmingController);
-				swimmerView.setVisible(true);
+								new MongoClient(new ServerAddress(mongoHost, mongoPort)), 
+								databaseName, collectionName);
+				SwimmerSwingView swimmerSwingView = new SwimmerSwingView();
+				SwimmingController swimmingController = new SwimmingController(swimmerSwingView, swimmerRepository);
+				swimmerSwingView.setSwimmingController(swimmingController);
+				swimmerSwingView.setVisible(true);
 				swimmingController.allSwimmers();
 			} catch(Exception e) {
-				e.printStackTrace();
+				Logger.getLogger(getClass().getName()).log(Level.SEVERE, "Exception", e);
 			}
 		});
+		return null;
 	}
 }
